47aa0d95c8fcd1f70a9e53c19df61338d47e8dd1
SingularityKChen <chency_singularity@163.com>
2019-03-15 16:26:22 +0800

    Update tree_attain_TOT.pl
    Replace the dir with $user and $runsrv

--- EDIT5/tree_attain_TOT.pl
+++ EDIT5/tree_attain_TOT.pl
@@ -1,31 +1,39 @@
 #!/usr/bin/perl 
 # Filename: tree_attain_TOT.pl
 # Author: SingularityKChen
-# Date: 2019.03.04
-# Edition: V5.1.X
+# Date: 2019.03.12
+# Edition: V5.5.X
 # THIS IS FOR TOT TREE WHITHOUT TAG.
+# New:	Version: V5.5.X
+#		Replace the dir with $user and $runsrv
 use strict; 
 use Getopt::Long; 
-unshift (@INC, "");
+use warnings;
+unshift (@INC, "/home/cn9259/bin/script/");
 require("chip_readinfo.pl");
+require("host_user.pl");
 my ($arch, $chip, $branch, $filename);
 GetOptions (
             'filename|f=s'    =>\$filename,
             );
 
-my (%tadb, %module_test_group);
-my ($tadb_ref, $module_test_group_ref);
+my ($tadb_ref, $module_test_group_ref, %tadb, %module_test_group);
 ($tadb_ref, $module_test_group_ref) = chip_readinfo($filename);
 %tadb = %$tadb_ref;
 %module_test_group = %$module_test_group_ref;
 
+my ($user_ref, $runsrv_ref, $user, $runsrv);
+($user_ref, $runsrv_ref) = host_user();
+$user = $$user_ref;
+$runsrv = $$runsrv_ref;
+
 $arch = $tadb{'arch'};
-$chip = $tadb{'chip'};
+$chip = $tadb{'def_chip'};
 $branch = $tadb{'branch'};
 
 my $tree_attain_dir = "";
-my $filefolder_name = "";
-my $p4_dir = "";
+my $filefolder_name = $user."_".$filename;
+my $p4_dir = $tree_attain_dir."";
 
 chdir($tree_attain_dir);
 if (-e $filefolder_name) {
@@ -33,12 +41,16 @@ if (-e $filefolder_name) {
 } else {
 	mkdir($filefolder_name);
 }
-my $p4_result = system "/bin/tcsh -c ''";
-if ( $p4_result = 0) {
-	print $filename."p4 successful".$p4_result."\n";
+my $get_tree_result = system "/bin/tcsh -c ''";
+if ( $get_tree_result = 0) {
+	print "**************\n $filefolder_name get_tree_result successful\n**************\n";
 	my $makerelease_result = system "/bin/tcsh -c ''";
-	print $makerelease_result."\n";
-}else{
-	print $filename."p4 fault".$p4_result."\n";
+	if ($makerelease_result == 0) {
+		print "**************\n $filefolder_name makerelease successful\n**************\n";
+	} else {
+		print "**************\n $filefolder_name makerelease fault\n**************\n";
+	}
+} else {
+	print "**************\n $filefolder_name get_tree_result fault\n**************\n";
 	break;
 }
