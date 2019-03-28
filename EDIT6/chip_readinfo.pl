757cf13886e672e79a2c4e5f0ec007104fb9c10f
SingularityKChen <chency_singularity@163.com>
2019-03-28 14:26:23 +0800

    Update chip_readinfo.pl
    Can read usc now

--- EDIT6/chip_readinfo.pl
+++ EDIT6/chip_readinfo.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl 
 # Filename: chip_readinfo.pl
 # Author: SingularityKChen
-# Date: 2019.03.14
-# Edition: V3.0
+# Date: 2019.03.27
+# Edition: V3.2
 #************************#
 #******** NEW ***********#
+#** Version: V3.2
+#** Date: 2019.03.27
+#* *Can read usc now;
+#
 #** Version: V3.0
+#** Date: 2019.03.14
 #* *Standardized the output of log;
 #
 #** Version: V2.0
@@ -18,11 +23,11 @@ sub chip_readinfo {
 	my $filename = $_[0];
 	my $list_dir = "";
 	my (@lines, %tadb, %module_test_group);
-	print "[*I*N*F*O*] Reading chip information from $list_dir$filename\n";
+	print "\n--------------\n[*I*N*F*O*] Reading chip information from $list_dir$filename\n--------------\n";
 	open(Fin,"<", $list_dir.$filename)  || die "[*E*R*R*O*R*] Can't open $filename:$!\n";
 	@lines = <Fin>;
 	close (Fin);
-	print "\n[*I*N*F*O*] the chip information is as following\n********************\n";
+	print "\n--------------\n[*I*N*F*O*] the chip information is as following\n--------------\n";
 	foreach my $readinfo (@lines){
 		chomp($readinfo);
 		my $tadb_exist = grep(/:/, $readinfo);
@@ -42,13 +47,13 @@ sub chip_readinfo {
 			my @tempsplit = split("\t",$readinfo);
 			$tempsplit[0] =~ s/(^\s+|\s+$)//g;
 			$tempsplit[1] =~ s/(^\s+|\s+$)//g;
-			if ($tempsplit[0] ne "Module" && $tempsplit[0] ne "usc") {
+			if ($tempsplit[0] ne "Module") {
 				$module_test_group{$tempsplit[1]} = $tempsplit[0];
 				print "$tempsplit[1] = $module_test_group{$tempsplit[1]}\n";
 			}
 		}
 	}
-	print "********************\n[*I*N*F*O*] read info finished\n";
+	print "--------------\n[*I*N*F*O*] read info finished\n--------------\n";
 	return( \%tadb, \%module_test_group );
 }
 1;
