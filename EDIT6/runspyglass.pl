08d73d473e20745dddb09082339a02eeee821093
SingularityKChen <chency_singularity@163.com>
2019-03-28 14:42:23 +0800

    Update and rename runspyglass.pl.pl to runspyglass.pl
    #** Version: V3.6
    #** Date: 2019.03.18
    #* *Fix the bug that the $datestring
    #   equals to the starting time;
    #* *Add runlog of testgroup;
    #* *Better the way to see if the results 
    #   is successful rather than being closed;

--- EDIT6/runspyglass.pl
+++ EDIT6/runspyglass.pl
@@ -1,14 +1,24 @@
 #!/usr/bin/perl 
 # Filename: runspyglass.pl
 # Author: SingularityKChen
-# Date: 2019.03.15
-# Edition: V3.5
-# Servise: srv200
-# FOR the VIP chip only because of the env
-# You are supposed to correct the $cp_rtl_dir
+# Date: 2019.03.18
+# Edition: V3.6
+# Servise: ***
+# FOR the *** only because of the env;
+# You are supposed to correct the $cp_rtl_dir;
+# postdft.pl are needed if you're going to run dft;
 #************************#
 #******** NEW ***********#  
+#** Version: V3.6
+#** Date: 2019.03.18
+#* *Fix the bug that the $datestring
+#   equals to the starting time;
+#* *Add runlog of testgroup;
+#* *Better the way to see if the results 
+#   is successful rather than being closed;
+#
 #** Version: V3.5
+#** Date: 2019.03.15
 #* *Add dft to this script;
 #* *Rename it as runspyglass.pl;
 #* *[2] for dft;
@@ -29,7 +39,7 @@ use Getopt::Long;
 use warnings;
 use POSIX ":sys_wait_h";
 use POSIX qw(strftime);
-unshift (@INC, "");
+unshift (@INC, "***");
 require("chip_readinfo.pl");
 
 my ($tag, $arch, $chip, $branch, $filename, $subrtl_file, $staging_file);
@@ -42,26 +52,27 @@ my ($tadb_ref, $module_test_group_ref, %tadb, %module_test_group);
 ($tadb_ref, $module_test_group_ref) = chip_readinfo($filename);
 %tadb = %$tadb_ref;
 %module_test_group = %$module_test_group_ref;
-
+my $datestring;
 $tag = $tadb{'tag'};
 $arch = $tadb{'arch'};
 $chip = $tadb{'def_chip'};
 $branch = $tadb{'branch'};
 $subrtl_file = $tadb{'subrtl'};
 $staging_file = $tadb{'staging'};
-my $datestring = strftime "%Y_%m_%d__%H_%M", localtime;
 
-my $cp_rtl_dir = "";
+my $cp_rtl_dir = "***";
 
 if (-e $cp_rtl_dir) {
-	print "[*I*N*F*O*] cp rtl dir right at $cp_rtl_dir\n";
+	print "--------------\n[*I*N*F*O*] cp rtl dir right at $cp_rtl_dir\n--------------\n";
 } else {
-	die "[*E*R*R*O*R*] cp rtl dir do not exists!\nThe time is $datestring\n";
+
+	$datestring = strftime "%Y_%m_%d__%H_%M", localtime;
+	die "--------------\n[*E*R*R*O*R*] cp rtl dir do not exists!\nThe time is $datestring\n--------------\n";
 }
 
-my $waiver_list_dir = "";
-my $filelist_head = "";
-my $filelist_tail = "";
+my $waiver_list_dir = "***";
+my $filelist_head = "***";
+my $filelist_tail = "***";
 
 my (@cp_env_dir, @run_dir, @env_prepare_code, @env_prepare_results, @source_result);
 my @prc = ("cdc", "lint", "dft");
@@ -74,31 +85,33 @@ my $collect;#signal of collect
 $SIG{CHLD} = sub { $num_proc-- };#
 
 for (my $i = 0; $i < 3; $i++) {
+#	my $i = 2;
 	my $pid = fork();#fork a new process
 	if (!defined($pid)) {
-	    print "[*E*R*R*O*R*] Error in fork: $!\nThe time is $datestring\n";
+		$datestring = strftime "%Y_%m_%d__%H_%M", localtime;
+	    print "--------------\n[*E*R*R*O*R*] Error in fork: $!\nThe time is $datestring\n--------------\n";
 	    exit 1;
 	}
 	if ($pid == 0) {
-		print "[*I*N*F*O*] Child $i : My pid = $$\n";
-		$cp_env_dir[$i] = "";
-		$run_dir[$i] = "";
+		print "--------------\n[*I*N*F*O*] Child $i : My pid = $$\n--------------\n";
+		$cp_env_dir[$i] = "***";
+		$run_dir[$i] = "***";
 		chdir($run_dir[$i]);
 		if (-e $run_dir[$i]) {
-			print "[*I*N*F*O*] $run_dir[$i] exists!\n";
+			print "--------------\n[*I*N*F*O*] $run_dir[$i] exists!\n--------------\n";
 		} else {
 			mkdir($run_dir[$i]);
 		}
 		if ($i == 2) {
-			$env_prepare_code[2] = "";
+			$env_prepare_code[2] = "***";
 		} else {
-			$env_prepare_code[$i] = "";
+			$env_prepare_code[$i] = "***";
 		}
 		print $env_prepare_code[$i];
-
 		$env_prepare_results[$i] = system("/bin/tcsh -c '$env_prepare_code[$i]'");
+		$datestring = strftime "%Y_%m_%d__%H_%M", localtime;
 		if ($env_prepare_results[$i] == 0) {
-			print "--------------\n[*I*N*F*O*] env_prepare_results $i successful\ntag is $tag\n--------------\n";
+			print "\n--------------\n[*I*N*F*O*] env_prepare_results $i successful\ntag is $tag\n--------------\n";
 			chdir($run_dir[$i]);
 			my @tmplist = glob "rtl/$subrtl_file/*.v";
 			open(FILELIST, ">", "file_list") || die "[*E*R*R*O*R*] Can't open file_list at $run_dir[$i] $!\nThe time is $datestring\n";
@@ -108,32 +121,35 @@ for (my $i = 0; $i < 3; $i++) {
 			}
 			print FILELIST $filelist_tail;
 			close(FILELIST);
-			my $sourcecode = "".$prc[$i];
+			my $sourcecode = "***";
 			my @source_result;
-			#$source_result[$i] = 0;
-			$source_result[$i] = system "/bin/tcsh -c 'gnome-terminal --title=\"$prc[$i]\" -x tcsh -c \"$sourcecode;exec tcsh\" '";
-			if ($source_result[$i] == 0) {
-				print "--------------\n[*I*N*F*O*] source $prc[$i] successful\ntag is $tag\n--------------\n";
+			$source_result[$i] = system "/bin/tcsh -c 'gnome-terminal --title=\"$prc[$i]\" -x tcsh -c \"$sourcecode; mkdir -p successful/$prc[$i]; exec tcsh\" '";
+			$datestring = strftime "%Y_%m_%d__%H_%M", localtime;
+			if ($source_result[$i] == 0 && -e "successful/$prc[$i]") {
+				print "\n--------------\n[*I*N*F*O*] source $prc[$i] successful\ntag is $tag\n--------------\n";
 				if ($i == 2) {
-					my $postdft_result = system "/bin/tcsh -c 'gnome-terminal --title=\"$prc[$i] postdft\" -x tcsh -c \"~/bin/script/postdft.pl -r $run_dir[$i];exec tcsh\" '";
-					if ($postdft_result == 0) {
-						print "--------------\n[*I*N*F*O*] source $prc[$i] postdft successful\ntag is $tag\n--------------\n";
+					my $postdft_result = system "/bin/tcsh -c '~/bin/script/postdft.pl -r $run_dir[$i]; mkdir -p successful/postdft;exec tcsh'";
+					$datestring = strftime "%Y_%m_%d__%H_%M", localtime;
+					if ($postdft_result == 0 && -e "successful/postdft") {
+						print "\n--------------\n[*I*N*F*O*] source $prc[$i] postdft successful\ntag is $tag\n--------------\n";
 					}else {
-						print "--------------\n[*E*R*R*O*R*] source $prc[$i] postdft fault\ntag is $tag\nThe time is $datestring\n--------------\n";
+						print "\n--------------\n[*E*R*R*O*R*] source $prc[$i] postdft failed\ntag is $tag\nThe time is $datestring\n--------------\n";
 					}
 				}
 			} else {
-				print "--------------\n[*E*R*R*O*R*] source $prc[$i] fault\ntag is $tag\nThe time is $datestring\n--------------\n";
+				print "\n--------------\n[*E*R*R*O*R*] source $prc[$i] failed\ntag is $tag\nThe time is $datestring\n--------------\n";
 			}
 		} else {
-			print "--------------\n[*E*R*R*O*R*] env_prepare_results $i fault\ntag is $tag\nThe time is $datestring\n--------------\n";
+			print "\n--------------\n[*E*R*R*O*R*] env_prepare_results $i failed\ntag is $tag\nThe time is $datestring\n--------------\n";
 		}
 		exit 0;
 	}
+#=pot
 	$num_proc ++;
 	if (($i-$num_proc-$num_collect) > 0) {
 	    while (($collect = waitpid(-1, WNOHANG)) > 0) {
 	        $num_collect ++;
+	        $datestring = strftime "%Y_%m_%d__%H_%M", localtime;
 	        print "--------------\n[*I*N*F*O*] Some one has FINISHED\nThe time is $datestring\n--------------\n";
 	    }
 	}
@@ -143,4 +159,5 @@ for (my $i = 0; $i < 3; $i++) {
 	}
 	print $i;
 }
+#=cut
 exit 0;
